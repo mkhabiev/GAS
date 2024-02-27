e47a80a347584813c4da9fc3b8a2ee92c6c29529
Mukhammed <m_khabiev@mail.ru>
2024-02-27 10:52:35 +0300

    Init Ability Actor Info

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -33,3 +33,11 @@ void AAuraEnemy::UnHighlightActor()
 	GetMesh()->SetRenderCustomDepth(false);
 	Weapon->SetRenderCustomDepth(false);
 }
+
+void AAuraEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AbilitySystemComponent)
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+}
